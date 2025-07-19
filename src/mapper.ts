780c106b04736847704354e3be3fbfa4c8db28a7
Bludwarf <bludwarf@gmail.com>
2025-07-19 14:06:31 +0200

    feat: CMB

--- src/mapper.ts
+++ src/mapper.ts
@@ -1,11 +1,11 @@
 import {QifTransaction} from "qif-ts/dist/types";
 
 export interface Mapper<I extends CSVRow> {
-    printHeaders(): void;
+  printHeaders(): void;
 
-    printRow(rowNr: number, row: I): void;
+  printRow(rowNr: number, row: I): void;
 
-    map(rowNr: number, row: I): QifTransaction;
+  map(rowNr: number, row: I): QifTransaction;
 }
 
 export interface CSVRow extends Record<string, any> {
@@ -13,8 +13,8 @@ export interface CSVRow extends Record<string, any> {
 }
 
 export interface QIFRow extends Record<string, string> {
-    d: string;
-    m?: string;
-    t: string;
-    p: string;
+  d: string;
+  m?: string;
+  t: string;
+  p: string;
 }
